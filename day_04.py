1885de851b5f92fb27267080a3b1fec4630f2822
Filip <filip.han@interia.pl>
2022-07-04 18:52:52 +0200

    day 4

--- day_04.py
+++ day_04.py
@@ -1,67 +1,3 @@
-class SquidBingo:
-    def __init__(self, boards, draw_order):
-        self.boards = boards
-        self.draw_order = draw_order
-
-        self.rows = len(boards[0])
-        self.cols = len(boards[0][0])
-        self.is_winning = [False] * len(boards)
-        self.flagged = [[[False] * len(row) for row in board] for board in boards]
-
-    def _check_if_wins(self, board_index):
-        for row in self.flagged[board_index]:
-            if sum(row) == self.rows:
-                return True
-
-        for column in zip(*self.flagged[board_index]):
-            if sum(column) == self.cols:
-                return True
-
-        return False
-
-    def _flag(self, board_index, number):
-        for row_index in range(self.rows):
-            for col_index in range(self.cols):
-                if self.boards[board_index][row_index][col_index] == number:
-                    self.flagged[board_index][row_index][col_index] = True
-
-    def _calculate_score(self, board_index, last_number):
-        unmarked_sum = 0
-
-        for row_index in range(self.rows):
-            for col_index in range(self.cols):
-                if not self.flagged[board_index][row_index][col_index]:
-                    unmarked_sum += self.boards[board_index][row_index][col_index]
-
-        return unmarked_sum * last_number
-
-    def solve(self):
-        first_score = None
-        last_score = None
-
-        for number in self.draw_order:
-
-            if all(self.is_winning):
-                break
-
-            for board_index in range(len(self.boards)):
-
-                if self.is_winning[board_index]:
-                    continue
-
-                self._flag(board_index, number)
-
-                if self._check_if_wins(board_index):
-                    score = self._calculate_score(board_index, number)
-                    if first_score is None:
-                        first_score = score
-                    last_score = score
-
-                    self.is_winning[board_index] = True
-
-        return first_score, last_score
-
-
 with open("day_04.in", "rt") as f:
     draw_order = [int(value) for value in f.readline().split(",")]
     boards = [
@@ -69,7 +5,65 @@ with open("day_04.in", "rt") as f:
         for board in f.read().strip().split("\n\n")
     ]
 
-bingo = SquidBingo(boards, draw_order)
-first_winning_score, last_winning_score = bingo.solve()
+
+board_count = len(boards)
+grid_size = len(boards[0])
+
+# Keep track of winning boards
+is_winning = [False] * board_count
+# Keep track of drawn numbers
+drawn = [[[False] * grid_size for row in board] for board in boards]
+
+
+first_winning_score = None
+last_winning_score = None
+
+for number in draw_order:
+
+    if all(is_winning):
+        break
+
+    for board_index in range(board_count):
+        # Do not check a board that is already winning
+        if is_winning[board_index]:
+            continue
+
+        # Set a flag that number was already chosen
+        for row_index in range(grid_size):
+            for col_index in range(grid_size):
+                if boards[board_index][row_index][col_index] == number:
+                    drawn[board_index][row_index][col_index] = True
+
+        # Search for full row or column
+        won = False
+        for row in drawn[board_index]:
+            if sum(row) == grid_size:
+                won = True
+                break
+        else:
+            for column in zip(*drawn[board_index]):
+                if sum(column) == grid_size:
+                    won = True
+                    break
+
+        if won:
+            # Mark board as winning
+            is_winning[board_index] = True
+
+            # Calculate score for the board
+            unmarked_sum = 0
+            for row_index in range(grid_size):
+                for col_index in range(grid_size):
+                    if not drawn[board_index][row_index][col_index]:
+                        unmarked_sum += boards[board_index][row_index][col_index]
+
+            score = unmarked_sum * number
+
+            # Save scores
+            if first_winning_score is None:
+                first_winning_score = score
+            last_winning_score = score
+
+
 print(f"{first_winning_score = }")
 print(f"{last_winning_score = }")
